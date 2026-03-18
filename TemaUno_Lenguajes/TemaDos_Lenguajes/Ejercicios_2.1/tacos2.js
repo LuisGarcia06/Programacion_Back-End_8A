9a32f15090b56041a262a85852d8a99d55a1fc44
LuisGarcia06 <221k0180@itscarrillopuerto.edu.mx>
2026-03-18 12:49:39 -0500

    Elimino TemaDos del lugar incorrecto

--- TemaUno_Lenguajes/TemaDos_Lenguajes/Ejercicios_2.1/tacos2.js
+++
@@ -1,8 +0,0 @@
-
-// Cadena JSON
-
-const jsonString = '{"nombre":"Taco de pollo", "Ingredientes":{"proteina": "Pollo", "Salsa": "Salsa Verde"}}';
-
-const objetoDeserializado = JSON.parse(jsonString);
-
-console.log(objetoDeserializado);
