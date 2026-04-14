64e39d14f3e0c06536652e33334f8a1f9689cf0e
LuisGarcia06 <221k0180@itscarrillopuerto.edu.mx>
2026-04-13 23:45:16 -0500

    eliminar TemaDos_Lenguajes duplicada de TemaUno_Lenguajes

---
+++ TemaDos_Lenguajes/AA_2.2_API/tokenPost.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const registrarAuth = async () => {
+    try {
+        const respuesta = await axios.post(
+        'https://dummyjson.com/auth/login',
+        {
+            username : 'emilys',
+            password : 'emilyspass',
+
+        },
+
+        {withCredentials: true}
+    );
+    console.log('Registro exitoso', respuesta.data);
+    } catch (error) {
+        console.error('Error en el registro: ', error.response.data)
+    }
+    
+}
+
+registrarAuth();
+
